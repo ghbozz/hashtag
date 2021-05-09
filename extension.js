d3bd7d482007b7c129e804290927f4ba971cea0b
romain sanson <romainsanson.gm@gmail.com>
2021-05-10 00:16:55 +0200

    clean

--- extension.js
+++ extension.js
@@ -1,6 +1,5 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-const { idText } = require('typescript');
 const vscode = require('vscode');
 
 // this method is called when your extension is activated
