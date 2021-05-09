763159608030e34fcac4a62a7fed52bd4d9d38df
romain sanson <romainsanson.gm@gmail.com>
2021-05-09 23:43:25 +0200

    interpolate only in string

--- extension.js
+++ extension.js
@@ -1,5 +1,6 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
+const { idText } = require('typescript');
 const vscode = require('vscode');
 
 // this method is called when your extension is activated
@@ -16,34 +17,46 @@ function activate(context) {
   vscode.commands.registerCommand('type', (args) => {
     const editor = vscode.window.activeTextEditor;
 
-    if (shouldInterpolate(editor, args)) {
-      return vscode.commands
-        .executeCommand('default:type', { text: '#{}' })
-        .then(() => {
-          setPosition(editor);
-        });
+    ruby = editor.document.languageId === 'ruby';
+    hashtag = args.text === '#';
+
+    if (ruby && hashtag) {
+      return vscode.commands.executeCommand('default:type', args).then(() => {
+        if (isInString(editor)) autocomplete(editor, args);
+      });
     }
     return vscode.commands.executeCommand('default:type', args);
   });
 
-  const setPosition = (editor) => {
-    const position = editor.selection.active;
-    const newPosition = position.with(position.line, position.character - 1);
-    editor.selection = new vscode.Selection(newPosition, newPosition);
-  };
-
-  const shouldInterpolate = (editor, args) => {
-    inString(editor); // WIP
-    return editor.document.languageId === 'ruby' && args.text === '#';
-  };
-
-  const inString = (editor) => {
-    // text = text value of active line
+  const isInString = (editor) => {
     const { text } = editor.document.lineAt(editor.selection.active.line);
-    // character = index value of cursor position
     const { character } = editor.selection.active;
 
-    return true;
+    const line = [
+      text.substring(0, character),
+      text.substring(character, text.length),
+    ];
+
+    const quotes = line.map(
+      (string) => string.split('').filter((c) => c === '"').length
+    );
+
+    return quotes[0] % 2 === 1 && quotes[1] % 2 === 1;
+  };
+
+  const autocomplete = (editor, args) => {
+    // console.log(editor.edit);
+    editor.edit((builder) => {
+      builder.replace(editor.selection, '{}');
+    });
+
+    cursor(editor);
+  };
+
+  const cursor = (editor) => {
+    const position = editor.selection.active;
+    const offset = position.with(position.line, position.character + 1);
+    editor.selection = new vscode.Selection(offset, offset);
   };
 }
 
